fd4b0fe702e9558bcb1c2d21337379d269522789
Ben Lubar <ben.lubar@gmail.com>
2013-05-09 22:33:38 -0500

    rewrite, courtesy of dustin

--- main.go
+++ main.go
@@ -2,58 +2,82 @@ package main
 
 import (
 	"fmt"
-	"github.com/dustin/go-couchbase"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/dustin/go-couchbase"
 )
 
+var bucket *couchbase.Bucket
+
+func processFile(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	base := filepath.Base(filename)
+
+	read := LogReader(f)
+
+	for {
+		ln, line, err := read()
+		if err != nil {
+			if err != io.EOF {
+				return err
+			}
+			return nil
+		}
+
+		line["Base"] = base
+		_, err = bucket.Add(fmt.Sprintf("%s+%d", base, ln), 0, line)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func processFiles(fns <-chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for filename := range fns {
+		err := processFile(filename)
+		if err != nil {
+			log.Fatalf("Error on %v: %v", filename, err)
+		}
+	}
+}
+
 func main() {
-	bucket, err := couchbase.GetBucket("http://localhost:8091/", "default", "default")
+	var err error
+	bucket, err = couchbase.GetBucket("http://localhost:8091/", "default", "default")
 	if err != nil {
 		panic(err)
 	}
 	defer bucket.Close()
 
-	var wg sync.WaitGroup
-	wg.Add(len(os.Args) - 1)
+	wg := &sync.WaitGroup{}
+
+	ch := make(chan string)
+
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go processFiles(ch, wg)
+	}
 
 	for i := 1; i < len(os.Args); i++ {
-		go func(i int) {
-			defer wg.Done()
-			filename, err := filepath.Abs(os.Args[i])
-			if err != nil {
-				panic(err)
-			}
-
-			f, err := os.Open(filename)
-			if err != nil {
-				panic(err)
-			}
-			defer f.Close()
-
-			base := filepath.Base(filename)
-
-			read := LogReader(f)
-
-			for {
-				ln, line, err := read()
-				if err != nil {
-					if err != io.EOF {
-						panic(err)
-					}
-					return
-				}
-
-				line["Base"] = base
-				_, err = bucket.Add(fmt.Sprintf("%s+%d", base, ln), 0, line)
-				if err != nil {
-					panic(err)
-				}
-			}
-		}(i)
+		abspath, err := filepath.Abs(os.Args[i])
+		if err != nil {
+			log.Fatalf("Error absing %v: %v", os.Args[i], err)
+		}
+		ch <- abspath
 	}
+	close(ch)
 
 	wg.Wait()
 }
